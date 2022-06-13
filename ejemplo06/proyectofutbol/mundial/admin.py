5874a78e7cda16cf628e5b62c2abfcdfee7676ea
Sofi Jaramillo <asjaramillo9@utpl.edu.ec>
2022-06-13 14:45:45 -0500

    Actualizacion Clase

---
+++ ejemplo06/proyectofutbol/mundial/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+from mundial.models import Estadio, Equipo
+
+admin.site.register(Estadio)
+admin.site.register(Equipo)
---
+++ proyectofutbol/proyectofubol/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProyectofubolConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'proyectofubol'
